de9b1439e52f6a2faad79c2ce12cdeda809d03b4
Homyku <damian.majerski4@gmail.com>
2023-02-13 21:47:36 +0100

    added rotating pizza, moving bckgrnd

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
 const audioElement = document.getElementById("myAudio");
 
 function playAudio() {
-  audioElement.play();
+audioElement.play();
 }
 
 function pauseAudio() {
-  audioElement.pause();
+audioElement.pause();
 }
+
+var y = 0;
+var speed = 2;
+var backgroundImage = new Image();
+backgroundImage.src = "./images/background.jpg";
+
+function moveBackground() {
+y -= speed;
+document.body.style.backgroundPosition = "0 " + y + "px";
+if (y < -backgroundImage.height / 4) {
+y = 0;
+}
+setTimeout(moveBackground, 50);
+}
+
+moveBackground();
+
+const CurrentTitle = document.title;
+
+window.addEventListener("blur", () => {
+  document.title= "Tej no wracaj no tu";
+
+});
+
+window.addEventListener("focus", () => {
+  document.title = CurrentTitle;
+});
